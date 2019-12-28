b4bde49d41240af5c52da582a4e72e5087ef6a03
Nargiz <nargiz1397@mail.ru>
2019-12-28 16:44:33 +0300

    finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :microposts
+  resources :users
+  #аправляет урлы определенного ресурса к соответсвюущему контроллеру
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  'application#hello'
+  # root  'application#hello'
+    root 'users#index'
 end
